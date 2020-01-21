baac9cf986d15b12563323b23597ca0568e51ceb
wangjun <1270317991@qq.com>
2020-01-21 13:55:05 +0800

    first commit

---
+++ src/test/java/com/wj/spring/cloud/eurekaserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wj.spring.cloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
